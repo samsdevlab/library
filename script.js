d367821757c2a58a1e82bcf62105cfb0c6ef4dfb
Sam Hadfield <sthtech@protonmail.com>
2025-03-21 17:34:35 -0500

    Create displayBooks function
    Create displayBooks function. This function iterates through the myLibrary
    array and adds a div to the DOM for each object found in the array.

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ function addBookToLibrary(title, author, pageCount, completionStatus, image) {
   let newBook = new Book(title, author, pageCount, completionStatus, image);
   myLibrary.push(newBook);
 }
+
+// Display Books - Loop Through Array
+function displayBooks(arr) {
+  const main = document.querySelector(".main");
+  arr.forEach((book) => {
+    const div = document.createElement("div");
+    div.classList.add("my-books");
+    main.appendChild(div);
+  });
+}
+
+// Call Default Books
+addBookToLibrary("Brave New World", "Aldous Huxley", "288", "Read");
+addBookToLibrary("Neuromancer", "William Gibson", "271", "Read");
+addBookToLibrary("1984", "George Orwell", "328", "Read");
+
+displayBooks(myLibrary);
