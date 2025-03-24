557255ae09827ec94f2ef6db73a5647707431968
Sam Hadfield <sthtech@protonmail.com>
2025-03-24 11:29:31 -0500

    Add background images to divs upon creation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const myLibrary = []; // book objects go here
 
 // The Constructor
 function Book(title, author, pageCount, completionStatus, image) {
+  // the constructor...
   if (!new.target) {
     throw Error("You must use the 'new' keyword to create a new object.");
   }
@@ -15,25 +16,39 @@ function Book(title, author, pageCount, completionStatus, image) {
   this.id = crypto.randomUUID();
 }
 
+
 // Create and Store Book
 function addBookToLibrary(title, author, pageCount, completionStatus, image) {
+  // take params, create a book then store it in the array
   let newBook = new Book(title, author, pageCount, completionStatus, image);
   myLibrary.push(newBook);
 }
 
 // Display Books - Loop Through Array
-function displayBooks(arr) {
+const displayBooks = function displayBooks(myLibraryArr) {
   const main = document.querySelector(".main");
-  arr.forEach((book) => {
+  myLibraryArr.forEach((book) => {
+
     const div = document.createElement("div");
     div.classList.add("my-books");
+    
+    const image = document.createElement("img");
+    bookImage = book.image;
+    image.src = bookImage;
+    div.appendChild(image)
+
     main.appendChild(div);
   });
+
+  // Add hover effect to div with an overlaid "holographic" background
+  // Add book info on top of holographic background
 }
 
 // Call Default Books
-addBookToLibrary("Brave New World", "Aldous Huxley", "288", "Read");
-addBookToLibrary("Neuromancer", "William Gibson", "271", "Read");
-addBookToLibrary("1984", "George Orwell", "328", "Read");
+addBookToLibrary("Brave New World", "Aldous Huxley", "288", "Read", "./images/brave-new-world-cover.jpg");
+addBookToLibrary("A Scanner Darkly", "Philip K. Dick", "304", "Unread", "./images/a-scanner-darkly.jpg");
+addBookToLibrary("1984", "George Orwell", "328", "Read", "./images/1984-cover.png");
 
 displayBooks(myLibrary);
+
+// console.log(myLibrary)
